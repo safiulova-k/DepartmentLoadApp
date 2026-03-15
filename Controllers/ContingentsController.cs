6be2659f011832a14d6b06be9110741b3371dd3e
kamilia <Admin@DESKTOP-NFSG25N>
2026-03-15 19:33:35 +0400

    Добавлена таблица работы с контингентом

--- Controllers/ContingentsController.cs
+++ Controllers/ContingentsController.cs
@@ -1,12 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DepartmentLoadApp.Models.Contingent;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DepartmentLoadApp.Controllers
 {
-    public class ContingentsController : Controller
+    public class ContingentController : Controller
     {
+        private static ContingentPageViewModel _storage = CreateInitialModel();
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(_storage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(ContingentPageViewModel model)
+        {
+            if (model == null || model.Rows == null || model.Rows.Count == 0)
+            {
+                TempData["ErrorMessage"] = "Не удалось сохранить таблицу";
+                return RedirectToAction(nameof(Index));
+            }
+
+            NormalizeRows(model);
+
+            _storage = model;
+
+            TempData["SuccessMessage"] = "Таблица контингента сохранена";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private static ContingentPageViewModel CreateInitialModel()
+        {
+            return new ContingentPageViewModel
+            {
+                Rows = new List<ContingentDirectionRowViewModel>
+                {
+                    new()
+                    {
+                        DirectionCode = "09.03.04",
+                        IsBachelor = true,
+                        IsMaster = false,
+                        Course1Count = 0,
+                        Course2Count = 0,
+                        Course3Count = 0,
+                        Course4Count = 0
+                    },
+                    new()
+                    {
+                        DirectionCode = "09.03.03",
+                        IsBachelor = true,
+                        IsMaster = false,
+                        Course1Count = 0,
+                        Course2Count = 0,
+                        Course3Count = 0,
+                        Course4Count = 0
+                    },
+                    new()
+                    {
+                        DirectionCode = "09.04.04",
+                        IsBachelor = false,
+                        IsMaster = true,
+                        Course1Count = 0,
+                        Course2Count = 0,
+                        Course3Count = 0,
+                        Course4Count = 0
+                    },
+                    new()
+                    {
+                        DirectionCode = "09.04.03",
+                        IsBachelor = false,
+                        IsMaster = true,
+                        Course1Count = 0,
+                        Course2Count = 0,
+                        Course3Count = 0,
+                        Course4Count = 0
+                    },
+                    new()
+                    {
+                        DirectionCode = "09.04.03 БИ",
+                        IsBachelor = false,
+                        IsMaster = true,
+                        Course1Count = 0,
+                        Course2Count = 0,
+                        Course3Count = 0,
+                        Course4Count = 0
+                    }
+                }
+            };
+        }
+
+        private static void NormalizeRows(ContingentPageViewModel model)
+        {
+            var defaults = CreateInitialModel().Rows;
+
+            for (int i = 0; i < model.Rows.Count && i < defaults.Count; i++)
+            {
+                model.Rows[i].DirectionCode = defaults[i].DirectionCode;
+                model.Rows[i].IsBachelor = defaults[i].IsBachelor;
+                model.Rows[i].IsMaster = defaults[i].IsMaster;
+
+                if (model.Rows[i].Course1Count < 0) model.Rows[i].Course1Count = 0;
+                if (model.Rows[i].Course2Count < 0) model.Rows[i].Course2Count = 0;
+                if (model.Rows[i].Course3Count < 0) model.Rows[i].Course3Count = 0;
+                if (model.Rows[i].Course4Count < 0) model.Rows[i].Course4Count = 0;
+            }
         }
     }
 }
---
+++ Models/Contingent/ContingentPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DepartmentLoadApp.Models.Contingent
+{
+    public class ContingentPageViewModel
+    {
+        public List<ContingentDirectionRowViewModel> Rows { get; set; } = new();
+
+        // суммы по курсам
+        public int TotalCourse1 => Rows.Sum(x => x.Course1Count);
+        public int TotalCourse2 => Rows.Sum(x => x.Course2Count);
+        public int TotalCourse3 => Rows.Sum(x => x.Course3Count);
+        public int TotalCourse4 => Rows.Sum(x => x.Course4Count);
+
+        // общее количество студентов
+        public int TotalStudents =>
+            TotalCourse1 +
+            TotalCourse2 +
+            TotalCourse3 +
+            TotalCourse4;
+
+        // бакалавры
+        public int BachelorCourse1 => Rows.Where(x => x.IsBachelor).Sum(x => x.Course1Count);
+        public int BachelorCourse2 => Rows.Where(x => x.IsBachelor).Sum(x => x.Course2Count);
+        public int BachelorCourse3 => Rows.Where(x => x.IsBachelor).Sum(x => x.Course3Count);
+        public int BachelorCourse4 => Rows.Where(x => x.IsBachelor).Sum(x => x.Course4Count);
+
+        // магистры
+        public int MasterCourse1 => Rows.Where(x => x.IsMaster).Sum(x => x.Course1Count);
+        public int MasterCourse2 => Rows.Where(x => x.IsMaster).Sum(x => x.Course2Count);
+        public int MasterCourse3 => Rows.Where(x => x.IsMaster).Sum(x => x.Course3Count);
+        public int MasterCourse4 => Rows.Where(x => x.IsMaster).Sum(x => x.Course4Count);
+    }
+}
