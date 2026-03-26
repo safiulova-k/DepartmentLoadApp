6c9772b34565d6ed7682fd3cbd4e7f9c34bbc818
kamilia <Admin@DESKTOP-NFSG25N>
2026-03-26 15:05:55 +0400

    удалила авторизацию, она будет через главный портал

--- Program.cs
+++ Program.cs
@@ -15,10 +15,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=WorkloadCalculation}/{action=Index}/{id?}");
 
 app.Run();
