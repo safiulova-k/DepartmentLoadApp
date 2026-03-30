2ac12b888879de969f8aa7dd404e3c1df76676d2
kamilia <Admin@DESKTOP-NFSG25N>
2026-03-30 22:03:47 +0400

    чуть допилила расчет дисциплин

--- ViewModels/Workload/WorkloadTablePageViewModel.cs
+++ ViewModels/Workload/WorkloadTablePageViewModel.cs
@@ -7,13 +7,5 @@ namespace DepartmentLoadApp.ViewModels.Workload
         public int SelectedYear { get; set; }
 
         public List<WorkloadRow> Rows { get; set; } = new();
-
-        public decimal TotalLectureHours => Rows.Sum(x => x.LectureTotalHours);
-
-        public decimal TotalPracticeHours => Rows.Sum(x => x.PracticeTotalHours);
-
-        public decimal TotalLabHours => Rows.Sum(x => x.LabTotalHours);
-
-        public decimal GrandTotalHours => TotalLectureHours + TotalPracticeHours + TotalLabHours;
     }
 }
