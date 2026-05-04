1122c13c162619fae52064be2982ff08ae63463a
kamilia <Admin@DESKTOP-NFSG25N>
2026-05-04 22:49:51 +0400

    бд ломается(

--- Migrations/20260414193228_AddStudentCountToStudentGroups.cs
+++ Migrations/20260414193228_AddStudentCountToStudentGroups.cs
@@ -8,12 +8,19 @@ namespace DepartmentLoadApp.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<int>(
+                name: "StudentCount",
+                table: "StudentGroupsCore",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "StudentCount",
+                table: "StudentGroupsCore");
         }
     }
 }
